6f29de5d864f71c406588d8c29e502a76c90a3ae
Harish R S <harishrs21082006@gmail.com>
2026-02-09 11:51:37 +0530

    feat audio upload working end-to-end

--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,10 @@ const app = express();
 const PORT = 5000;
 app.use(cors());
 app.use(express.json());
+app.use((req, res, next) => {
+  console.log("REQ:", req.method, req.url);
+  next();
+});
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 const storage = multer.diskStorage({
